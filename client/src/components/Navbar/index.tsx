dbef06a46edaed112c4c5db322766f9de7d1e315
alfredoizdev <alfredodevelop@gmail.com>
2023-01-17 18:06:52 -0600

    added button for fetch quotes on navbar

--- client/src/components/Navbar/index.tsx
+++ client/src/components/Navbar/index.tsx
@@ -1,10 +1,18 @@
-import { FunctionComponent } from 'react';
+import { AppContext } from 'context';
+import { FunctionComponent, useContext } from 'react';
 import styles from './Navbar.module.scss';
 
 const Navbar: FunctionComponent = () => {
+
+	const { setQuotesViewStatus } = useContext(AppContext);
+
 	return (
 		<div className={styles.navbarContainer}>
 			<h2>Insubuy app</h2>
+			<button
+				type="button"
+				onClick={() => setQuotesViewStatus(true)}
+			>Open Quotes</button>
 		</div>
 	);
 };
